94fb2cbd5e402a4e9cb0ac35a31ec383f32a885b
Jack Ooi <67252332+Ravenslayer998@users.noreply.github.com>
2023-10-06 10:50:55 +0800

    new file:   src/Components/Footeer.js 	new file:   src/Components/Footer.css 	new file:   src/Components/Header.css 	new file:   src/Components/Header.js 	modified:   src/index.js

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import './Components/Header.css'
+
+function Header(){
+    return(
+        <div className='Welcome'>
+            <h1>Welcome to my Calculator</h1>
+            <p>Please explore around and have fun !!</p>
+        </div>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import "./styles.css";
+import Header from "./Components/Header";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header/>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
