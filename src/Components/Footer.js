47ae34fe6e8f68871768df5ec1c3b24643b918df
Jack Ooi <67252332+Ravenslayer998@users.noreply.github.com>
2023-10-06 15:14:01 +0800

    modified:   package-lock.json 	modified:   package.json 	new file:   public/github-sign.png 	modified:   src/Components/Footer.css 	modified:   src/Components/Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,24 @@
 import './Footer.css';
+import {Link} from "react-router-dom"
+
+function getCurrentYear(){
+    const currentDate = new Date();
+    return currentDate.getFullYear();
+
+}
 
 const Footer = () => {
     return (
       <footer>
         <div className="container">
-          <p>Your Website Name</p>
+          <p>Copyright @ {getCurrentYear()}</p>
+        </div>
+          
+        <div className='Github Image'>
+            <button>
+            <a onClick='https://github.com/Ravenslayer998/calculator' rel='noopener' target = 'blank'></a>
+            <img src='./github-sign.png'></img>
+        </button>
         </div>
       </footer>
     );
