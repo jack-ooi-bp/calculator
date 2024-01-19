4cc85a5b9e0cc1ac66727607db20247f1056d4b4
jack.ooi <jack.ooi@bp.com>
2024-01-19 08:46:18 +0800

    Change formatting

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,34 +1,30 @@
 import { Box, HStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faLinkedin,
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
-function getCurrentYear(){
-    const currentDate = new Date();
-    return currentDate.getFullYear();
-
+function getCurrentYear() {
+  const currentDate = new Date();
+  return currentDate.getFullYear();
 }
 
 const socials = [
   {
     icon: faGithub,
-    url: 'https://github.com/Ravenslayer998/calculator',
+    url: "https://github.com/Ravenslayer998/calculator",
   },
 
   {
     icon: faLinkedin,
-    url: 'https://www.linkedin.com/in/thooi998/',
-  }
-]
+    url: "https://www.linkedin.com/in/thooi998/",
+  },
+];
 
 const Footer = () => {
-    return (
-      <footer className='Footer'>
-        <div className="Copyright">
-          <p>Copyright @ {getCurrentYear()}</p>
-        </div>
+  return (
+    <footer className="Footer">
+      <div className="Copyright">
+        <p>Copyright @ {getCurrentYear()}</p>
+      </div>
 
       <Box
         position="bottom"
@@ -36,26 +32,30 @@ const Footer = () => {
         left={0}
         right={0}
         translateY={0}
-
         backgroundColor="#18181b"
-        >
+      >
         <Box color="white" maxWidth="0 auto" margin="0 auto">
           <HStack
             px={16}
-            py={4}
+            py={5}
             justifyContent="flex-start"
             alignItems="center"
           >
-            <button>
-              <HStack spacing={8}>
-              {socials.map(({icon, url})=>(
+            <button className="button icon">
+              <HStack spacing={20}>
+                {socials.map(({ icon, url }) => (
                   <a
-                  key={url}
-                  href={url}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                    key={url}
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    <FontAwesomeIcon icon={icon} size="3x" key={url} color='white'/>
+                    <FontAwesomeIcon
+                      icon={icon}
+                      size="3x"
+                      key={url}
+                      color="white"
+                    />
                   </a>
                 ))}
               </HStack>
@@ -64,8 +64,7 @@ const Footer = () => {
         </Box>
       </Box>
     </footer>
-      
-    );
-  };
-  
-  export default Footer;
+  );
+};
+
+export default Footer;
